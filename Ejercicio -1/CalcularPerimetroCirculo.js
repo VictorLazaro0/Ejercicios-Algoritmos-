3e8825a8d54482fcf05134255f985a5dde4125a0
VictorLazaro <victormanuellazarobravo@gmail.com>
2026-08-21 16:10:23 -0600

    Prueba de modificacion del ejerciocio, no se cambio nada

--- Ejercicio -1/CalcularPerimetroCirculo.js
+++ Ejercicio -1/CalcularPerimetroCirculo.js
@@ -3,7 +3,6 @@ const prompt = require("prompt-sync")();
  let radio =parseInt(prompt("Ingresa el radio del circulo:"));
 function CalcularPerimetro(radio) {
     let perimetro = 2 * Math.PI * radio
-     
     return perimetro;
 }
 console.log(CalcularPerimetro(radio));
