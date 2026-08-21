f49e9579c2fd7528e6037b386329e4a5feb21d25
VictorLazaro <victormanuellazarobravo@gmail.com>
2026-08-21 14:24:40 -0600

    Se agrego el ejercicio Calcular el perimetro de un circulo en js

---
+++ Ejercicio -1/CalcularPerimetroCirculo.js
@@ -0,0 +1,9 @@
+
+const prompt = require("prompt-sync")();
+ let radio =parseInt(prompt("Ingresa el radio del circulo:"));
+function CalcularPerimetro(radio) {
+    let perimetro = 2 * Math.PI * radio
+     
+    return perimetro;
+}
+console.log(CalcularPerimetro(radio));
